89c0676c7c73b4ef4c2b6c13629f41f758514996
Vishnu K V <104137499+vishnu6363@users.noreply.github.com>
2023-01-22 10:21:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  9 22:09:42 2023
+
+@author: vishn
+"""
+
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+#import image
+from tensorflow.keras.preprocessing import image
+#import opencv
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://miro.medium.com/max/828/1*mUwYl3gW61G7pafyFnaCvw.gif");
+background-size: 180%;
+background-position: top;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+	model = tf.keras.models.load_model('./CNN(1).h5')
+	return model
+
+
+def predict_class(image, model):
+    
+    image = tf.cast(image, tf.float32)
+    #image = image.img_to_array(image)
+   # image=cv2.imread(image)
+    image = np.expand_dims(image, axis = 0)
+    prediction = model.predict(image)
+    return prediction
+
+
+model = load_model()
+st.title('Plant Disease Prediction')
+
+file = st.file_uploader("Upload an image of a flower")
+
+if file is None:
+	st.text('Waiting for upload....')
+
+else:
+	slot = st.empty()
+	slot.text('Running inference....')
+
+	test_image = tf.keras.preprocessing.image.load_img(file,target_size=(128,128))
+    
+    
+    
+
+	st.image(test_image, caption="Input Image", width = 200)
+
+	pred = predict_class(np.asarray(test_image), model)
+
+	class_names =['Apple___Apple_scab',
+               'Apple___Black_rot',
+               'Apple___Cedar_apple_rust',
+               'Apple___healthy',
+               'Blueberry___healthy',
+               'Cherry_(including_sour)___Powdery_mildew',
+               'Cherry_(including_sour)___healthy',
+               'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+               'Corn_(maize)___Common_rust_',
+               'Corn_(maize)___Northern_Leaf_Blight',
+               'Corn_(maize)___healthy',
+               'Grape___Black_rot',
+               'Grape___Esca_(Black_Measles)',
+               'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+               'Grape___healthy',
+               'Orange___Haunglongbing_(Citrus_greening)',
+               'Peach___Bacterial_spot',
+               'Peach___healthy',
+               'Pepper,_bell___Bacterial_spot',
+               'Pepper,_bell___healthy',
+               'Potato___Early_blight',
+               'Potato___Late_blight',
+               'Potato___healthy',
+               'Raspberry___healthy',
+               'Soybean___healthy',
+               'Squash___Powdery_mildew',
+               'Strawberry___Leaf_scorch',
+               'Strawberry___healthy',
+               'Tomato___Bacterial_spot',
+               'Tomato___Early_blight',
+               'Tomato___Late_blight',
+               'Tomato___Leaf_Mold',
+               'Tomato___Septoria_leaf_spot',
+               'Tomato___Spider_mites Two-spotted_spider_mite'
+               'Tomato___Target_Spot',
+               'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+               'Tomato___Tomato_mosaic_virus',
+               'Tomato___healthy']
+
+	result = class_names[np.argmax(pred)]
+
+	output = 'The Disease is ' + result
+
+	slot.text('Done')
+
+	st.success(output)
