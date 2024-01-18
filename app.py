fbd8a03f23ab2f9c5b129f5b5b8bea8fbeb0d2ea
Vishnu K V <104137499+vishnu6363@users.noreply.github.com>
2024-01-18 23:58:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan  9 22:09:42 2023
+Created on Mon Jan 9 22:09:42 2023
 
 @author: vishn
 """
@@ -9,78 +9,67 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import cv2
 
-#import image
-from tensorflow.keras.preprocessing import image
-#import opencv
-
-page_bg_img = f"""
+page_bg_img = """
 <style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://miro.medium.com/max/828/1*mUwYl3gW61G7pafyFnaCvw.gif");
-background-size: 180%;
-background-position: top;
-background-repeat: no-repeat;
-background-attachment: local;
-}}
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-[data-testid="stToolbar"] {{
-right: 2rem;
-}}
+[data-testid="stAppViewContainer"] > .main {
+    background-image: url("https://miro.medium.com/max/828/1*mUwYl3gW61G7pafyFnaCvw.gif");
+    background-size: 180%;
+    background-position: top;
+    background-repeat: no-repeat;
+    background-attachment: local;
+}
+[data-testid="stHeader"] {
+    background: rgba(0,0,0,0);
+}
+[data-testid="stToolbar"] {
+    right: 2rem;
+}
 </style>
 """
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
-
 @st.cache(allow_output_mutation=True)
 def load_model():
-	model = tf.keras.models.load_model('./convolutional_2D (1).h5')
-	return model
+    model = tf.keras.models.load_model('./convolutional_2D (1).h5')
+    return model
 
-
-
-
-
-def predict_class(image, model):
-    direct_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
-    target_size = (128, 128) 
-    direct_image = cv2.resize(direct_image, target_size) 
-    img = np.expand_dims(direct_image, axis=0) 
-    prediction = model.predict(img) 
+def predict_class(input_image, model):
+    direct_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+    target_size = (128, 128)
+    direct_image = cv2.resize(direct_image, target_size)
+    img = np.expand_dims(direct_image, axis=0)
+    prediction = model.predict(img)
     return prediction
 
+def main():
+    model = load_model()
+    st.title('Mongo Plant Disease Prediction')
 
-model = load_model()
-st.title('Mongo Plant Disease Prediction')
+    file = st.file_uploader("Upload an image of a Leaf")
 
-file = st.file_uploader("Upload an image of a Leaf")
+    if file is None:
+        st.text('Waiting for upload....')
+    else:
+        slot = st.empty()
+        slot.text('Running inference....')
 
-if file is None:
-	st.text('Waiting for upload....')
+        test_image = Image.open(file)
+        st.image(test_image, caption="Input Image")
 
-else:
-	slot = st.empty()
-	slot.text('Running inference....')
+        pred = predict_class(np.asarray(test_image), model)
 
-	test_image = tf.keras.preprocessing.image.load_img(file,target_size=(128,128))
-    
-    
-    
+        class_names = ['Anthracnose', 'Bacterial Canker', 'Cutting Weevil', 'Die Back', 'Gall Midge', 'Healthy',
+                       'Powdery Mildew', 'Sooty Mould']
 
-	st.image(test_image, caption="Input Image", width = 200)
+        result = class_names[np.argmax(pred, axis=1)]
 
-	pred = predict_class(np.asarray(test_image), model)
+        output = 'The Disease is ' + result
 
-	class_names =['Anthracnose', 'Bacterial Canker', 'Cutting Weevil', 'Die Back', 'Gall Midge', 'Healthy', 'Powdery Mildew', 'Sooty Mould']
+        slot.text('Done')
+        st.success(output)
 
-	result = class_names[np.argmax(pred,axis=1)]
-
-	output = 'The Disease is ' + result
-
-	slot.text('Done')
-
-	st.success(output)
+if __name__ == "__main__":
+    main()
