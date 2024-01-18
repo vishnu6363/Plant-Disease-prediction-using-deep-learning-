36052794ff7512eed817138b9b9b8a7394137163
Vishnu K V <104137499+vishnu6363@users.noreply.github.com>
2024-01-18 23:40:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,20 +42,22 @@ def load_model():
 	return model
 
 
+
+
+
 def predict_class(image, model):
-    
-    image = tf.cast(image, tf.float32)
-    #image = image.img_to_array(image)
-   # image=cv2.imread(image)
-    image = np.expand_dims(image, axis = 0)
-    prediction = model.predict(image)
+    direct_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+    target_size = (128, 128) 
+    direct_image = cv2.resize(direct_image, target_size) 
+    img = np.expand_dims(direct_image, axis=0) 
+    prediction = model.predict(img) 
     return prediction
 
 
 model = load_model()
-st.title('Plant Disease Prediction')
+st.title('Mongo Plant Disease Prediction')
 
-file = st.file_uploader("Upload an image of a flower")
+file = st.file_uploader("Upload an image of a Leaf")
 
 if file is None:
 	st.text('Waiting for upload....')
@@ -73,46 +75,9 @@ else:
 
 	pred = predict_class(np.asarray(test_image), model)
 
-	class_names =['Apple___Apple_scab',
-               'Apple___Black_rot',
-               'Apple___Cedar_apple_rust',
-               'Apple___healthy',
-               'Blueberry___healthy',
-               'Cherry_(including_sour)___Powdery_mildew',
-               'Cherry_(including_sour)___healthy',
-               'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
-               'Corn_(maize)___Common_rust_',
-               'Corn_(maize)___Northern_Leaf_Blight',
-               'Corn_(maize)___healthy',
-               'Grape___Black_rot',
-               'Grape___Esca_(Black_Measles)',
-               'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
-               'Grape___healthy',
-               'Orange___Haunglongbing_(Citrus_greening)',
-               'Peach___Bacterial_spot',
-               'Peach___healthy',
-               'Pepper,_bell___Bacterial_spot',
-               'Pepper,_bell___healthy',
-               'Potato___Early_blight',
-               'Potato___Late_blight',
-               'Potato___healthy',
-               'Raspberry___healthy',
-               'Soybean___healthy',
-               'Squash___Powdery_mildew',
-               'Strawberry___Leaf_scorch',
-               'Strawberry___healthy',
-               'Tomato___Bacterial_spot',
-               'Tomato___Early_blight',
-               'Tomato___Late_blight',
-               'Tomato___Leaf_Mold',
-               'Tomato___Septoria_leaf_spot',
-               'Tomato___Spider_mites Two-spotted_spider_mite'
-               'Tomato___Target_Spot',
-               'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
-               'Tomato___Tomato_mosaic_virus',
-               'Tomato___healthy']
+	class_names =['Anthracnose', 'Bacterial Canker', 'Cutting Weevil', 'Die Back', 'Gall Midge', 'Healthy', 'Powdery Mildew', 'Sooty Mould']
 
-	result = class_names[np.argmax(pred)]
+	result = class_names[np.argmax(pred,axis=1)]
 
 	output = 'The Disease is ' + result
 
