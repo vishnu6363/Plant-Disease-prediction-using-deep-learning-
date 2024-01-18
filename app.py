929b7f4e77f9d19e954cb94bcd1c02645c5bb35c
Vishnu K V <104137499+vishnu6363@users.noreply.github.com>
2024-01-19 00:44:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import cv2
+ 
  
 page_bg_img = """
 <style>
@@ -38,12 +38,12 @@ def load_model():
     return model
 
 def predict_class(input_image, model):
-   direct_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB) 
-   target_size = (128, 128) 
-   direct_image = cv2.resize(direct_image, target_size) 
-   img = np.expand_dims(direct_image, axis=0) 
-   prediction = model.predict(img) 
-   return prediction
+ # img = image.load_img(image_path)
+  img = image.img_to_array(input_image)
+  img = tf.image.resize(img, (128, 128)) 
+  direct_image = np.expand_dims(img, axis=0) 
+  prediction = model.predict(direct_image) 
+  return prediction
 
 def main():
     model = load_model()
