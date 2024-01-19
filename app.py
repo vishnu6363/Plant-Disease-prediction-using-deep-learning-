6806794067bd29e654274dace427fc383378bd51
Vishnu K V <104137499+vishnu6363@users.noreply.github.com>
2024-01-19 10:59:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 9 22:09:42 2023
+Created on Mon Jan  9 22:09:42 2023
+
 @author: vishn
 """
 
@@ -8,7 +9,10 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import cv2
+
+#import image
+from tensorflow.keras.preprocessing import image
+#import opencv
 
 page_bg_img = f"""
 <style>
@@ -30,52 +34,58 @@ right: 2rem;
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('./mangomodel.h5')
-    return model
+	model = tf.keras.models.load_model('convolutional.h5')
+	return model
 
-def preprocess_image(image):
-   direct_image = cv2.cvtColor(direct_image, cv2.COLOR_BGR2RGB) 
-   target_size = (128, 128) 
-   direct_image = cv2.resize(direct_image, target_size) 
-   img = np.expand_dims(direct_image, axis=0) 
-   return img
 
 def predict_class(image, model):
+    
+    image = tf.cast(image, tf.float32)
+    #image = image.img_to_array(image)
+   # image=cv2.imread(image)
+    image = np.expand_dims(image, axis = 0)
     prediction = model.predict(image)
     return prediction
 
-model = load_model()
-st.title('Mongo Plant Disease Prediction')
 
-file = st.file_uploader("Upload an image of a Mango Leaf")
+model = load_model()
+st.title('Plant Disease Prediction')
+
+file = st.file_uploader("Upload an image of a flower")
 
 if file is None:
-    st.text('Waiting for upload....')
+	st.text('Waiting for upload....')
+
 else:
-    slot = st.empty()
-    slot.text('Running inference....')
+	slot = st.empty()
+	slot.text('Running inference....')
 
-    # Load and preprocess the image
-    test_image = cv2.imread(file)
-    processed_image = preprocess_image(test_image)
+	test_image = tf.keras.preprocessing.image.load_img(file,target_size=(128,128))
+    
+    
+    
 
-    st.image(test_image, caption="Input Image", width=150)
+	st.image(test_image, caption="Input Image", width = 200)
 
-    pred = predict_class(processed_image, model)
-    print(pred)
+	pred = predict_class(np.asarray(test_image), model)
 
-    class_names = ['Anthracnose', 'Bacterial Canker', 'Cutting Weevil', 'Die Back', 'Gall Midge', 'Healthy',
-                   'Powdery Mildew', 'Sooty Mould']
+	class_names =['Anthracnose',
+ 'Bacterial Canker',
+ 'Cutting Weevil',
+ 'Die Back',
+ 'Gall Midge',
+ 'Healthy',
+ 'Powdery Mildew',
+ 'Sooty Mould']
 
-    # Get the class index with the highest probability
-    predicted_class_index = np.argmax(pred, axis=1)
-    # Get the corresponding class name
-    result = class_names[predicted_class_index]
+	result = class_names[np.argmax(pred)]
 
-    output = 'The Disease is ' + result
+	output = 'The Disease is ' + result
 
-    slot.text('Done')
+	slot.text('Done')
 
-    st.success(output)
+	st.success(output)
